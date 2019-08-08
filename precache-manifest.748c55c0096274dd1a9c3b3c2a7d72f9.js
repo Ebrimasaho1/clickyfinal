aa8474a707078572659aa5348d5d9a686d6abe98
Ebrima Saho <saho.ebrima@ymail.com>
2019-08-07 21:20:47 -0700

    Updates

---
+++ precache-manifest.748c55c0096274dd1a9c3b3c2a7d72f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0980247611d929aef6f3c48454cd6d09",
+    "url": "/github.io/clickyfinal/index.html"
+  },
+  {
+    "revision": "0aa1b64fc9519a442537",
+    "url": "/github.io/clickyfinal/static/css/main.e677d693.chunk.css"
+  },
+  {
+    "revision": "db0c3138e7824d4532a9",
+    "url": "/github.io/clickyfinal/static/js/2.3ec218cf.chunk.js"
+  },
+  {
+    "revision": "0aa1b64fc9519a442537",
+    "url": "/github.io/clickyfinal/static/js/main.5684dc83.chunk.js"
+  },
+  {
+    "revision": "97bfc209a73e0d9901ad",
+    "url": "/github.io/clickyfinal/static/js/runtime~main.a81e06a8.js"
+  },
+  {
+    "revision": "915c257c9a8e746eec8358dcd1d99c2e",
+    "url": "/github.io/clickyfinal/static/media/backImg.915c257c.jpg"
+  }
+]);
